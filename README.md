da45658cc8a8556ef35a93fea196223bd07154d2
brkckr20 <burak2043@gmail.com>
2022-08-15 17:08:50 +0300

    projeye baslandi

---
+++ README.md
@@ -0,0 +1,1 @@
+## JavaScript notlarini icerir.
---
+++ degiskenlerle-calismak/degisken-tanimlama.js
@@ -0,0 +1,42 @@
+/* var ile degisken tanimlamak */
+
+// var kullaniciAdi = "BurakCakir";
+// console.log(kullaniciAdi);
+
+
+/* let ile bos degisken tanimlamak */
+// let server;
+// console.log(server);
+
+/* let ile tanımlanmis olana deger atamak */
+// server = "https://google.com"
+// console.log(server);
+
+
+/* let ile degiskene bilgi atamak */
+// let password = 1234;
+// console.log(password);
+
+
+/* let ile atama yapmadan yazdirmak */
+// console.log(sifre);
+// let sifre = 2123;
+// ciktisi = Uncaught ReferenceError: Cannot access 'sifre' before initialization
+
+
+/* let ile tanimlanmis veriyi degistirmek */
+// let sifrem = "asdasdsdsa";
+// sifrem = 123;
+// console.log(sifrem);
+
+
+/* const ile ici boş değişken tanımlamak */
+/* const serverPassword;
+!! const ile tanımlama yaparken veri bos birakilamaz!!!! */
+
+
+/* const ile değişken tanımlamak */
+const serverPassword = 12344312;
+/* serverPassword = "abc"; */
+// const ile tanımlama yapıldığında daha sonra verinin değeri değiştirilemez!!!!!
+console.log(serverPassword);
