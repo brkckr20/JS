984ea26d49d16a89d38c43953f5071a07e60715e
Burak Çakır <burak2043@gmail.com>
2022-08-31 13:47:40 +0300

    kosul yapisi if else eklendi

---
+++ karar-yapilari-ve-kosullar/kosul-yapisi-if-else.js
@@ -0,0 +1,9 @@
+/* kullanıcı bilgisi varsa ekrana yazdir. */
+
+let username = "";
+
+if (username.length > 0) { /* if kısmı her zaman true ise çalışır */
+    console.log(username)
+} else {
+    console.log("Kullanıcı adi boş");
+}
