22a3f4fa186a6f7f5d7077bd5cd7e5674b49ce39
Burak Çakır <burak2043@gmail.com>
2022-09-19 12:21:05 +0300

    formdan veri alma eklendi

--- form/form-ve-form-submit.js
+++ form/form-ve-form-submit.js
@@ -1,7 +1,12 @@
 let formDom = document.querySelector("#user-form");
 formDom.addEventListener("submit", formSubmit);
 
+
 function formSubmit(event) {
     event.preventDefault(); // form un default sayfa yenileme işlemi iptal edildi.
+
+    let scoreInputDom = document.querySelector("#score");
+    // console.log(scoreInputDom.value); //form verisini almak
+    localStorage.setItem("score", scoreInputDom.value);
     console.log("form submit");
 }
