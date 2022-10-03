75ee619a521af088202d4fd0ad087c3d7f515ff9
Burak Çakır <burak2043@gmail.com>
2022-10-03 14:06:44 +0300

    objelere baslandi

---
+++ object/nesne-olusturma.js
@@ -0,0 +1,16 @@
+
+let arrayOBJ = [1, 2, 3];
+let obj1 = { obj: 1 };
+
+
+console.log("array obje", arrayOBJ);
+console.log("obje 1", obj1);
+
+
+
+//obje olusturma
+let item1 = new Object();
+let item2 = {};
+
+console.log("item 1", typeof item1);
+console.log("item 2", typeof item2);
