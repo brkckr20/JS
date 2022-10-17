89f07aa1120a628ab931add668c63e2c3d3f2eda
Burak Çakır <burak2043@gmail.com>
2022-10-17 14:30:54 +0300

    try catch eklendi

---
+++ try-catch/app.js
@@ -0,0 +1,16 @@
+let items;
+
+
+
+try {
+    items.forEach(item => {
+        console.log(item)
+    });
+} catch (error) {
+    console.log(error)
+}
+
+console.log("Hata Yönetimi");
+
+let info = "kodluyoruz"
+console.log(info)
