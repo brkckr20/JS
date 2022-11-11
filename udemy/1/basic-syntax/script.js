29f2b315e328548a6689e660e2bc7c3a5f2e48f0
Burak Çakır <burak2043@gmail.com>
2022-11-11 16:20:58 +0300

    basic syntax added

---
+++ udemy/1/basic-syntax/script.js
@@ -0,0 +1,46 @@
+//literals
+1
+
+1.2
+
+"test";
+
+//variables
+
+var a = 1;
+let b = "2";
+const c = 3.1
+
+console.log(a, b, c)
+
+//operators
+
+//değer atama operatörü
+const d = a - c;
+console.log(d);
+
+
+//expressions - ifadeler
+5 * 10
+"John Doe" + " Hi";
+
+
+//keywords - anahtar kelimeler
+//var
+//let
+//const
+//function
+//Date
+
+
+//comments - yorum satırları
+// asdasda
+/* asdasdasd */
+
+
+//identifier - isimlendirmeler
+/* 
+    camelCase
+    PascalCase
+    alt_cizgili
+*/
